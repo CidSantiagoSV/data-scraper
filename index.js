249c4aceecb63890b7228596db373849db44d4ad
cid <cid.santiago1@gmail.com>
2022-02-15 12:50:57 -0500

    Bare bones express application

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 ﻿const express = require('express');
+const request = require('request-promise');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.send('Welcome to data-scraper-api');
+});
+
+app.listen(PORT, () =>
+ console.log(`Server running on port ${PORT}`)
+);
